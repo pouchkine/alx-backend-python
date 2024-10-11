f4bcc11e76ae3ce45d40dd3661e2a2f39b56342f
pouch <poucheuwilfried1@gmail.com>
2024-10-11 05:17:13 -0700

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+add function with annotation
+"""
+
+
+def add(a: float, b: float) -> str:
+    """
+    a is a float
+    b is a float
+    return is a float
+    """
+    return a + b
